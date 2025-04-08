775366e642b259eb18043f1420f9d6c0ce9dc842
Vitalii <vitalii.gaponov@wirenboard.com>
2025-04-08 21:48:27 +0300

    feat: configure deb packet for python byild

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+def get_version():
+    with open("debian/changelog", "r", encoding="utf-8") as f:
+        return f.readline().split()[1][1:-1]
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="wb-mqtt-alice",
+    version=get_version(),
+    description="Yandex Alice to MQTT integration for Wiren Board controllers",
+    license="MIT",
+    author="Vitalii Gaponov",
+    author_email="vitalii.gaponov@wirenboard.com",
+    maintainer="Wiren Board Team",
+    maintainer_email="info@wirenboard.com",
+    url="https://github.com/wirenboard/wb-alice-client",
+    install_requires=requirements,
+    # Files installed by debian/install file
+)
