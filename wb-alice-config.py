79129eed5268b8fbf93d1bba177ed71315a7118f
BurchuMikhail <141926144+BurchuMikhail@users.noreply.github.com>
2025-05-20 10:49:47 +0300

    fix: fix the device transfer bug

--- wb-alice-config.py
+++ wb-alice-config.py
@@ -199,8 +199,8 @@ async def update_device(device_id: str, device_data: Device):
                       type=device_data.type,
                       capabilities=device_data.capabilities,
                       properties=device_data.properties)
-    config.devices[device_id] = response
     room_change(device_id, device_data.room_id, config)
+    config.devices[device_id] = response
     
     save_config(config)
     return response
