2e5b7a3f0e0ba2da31acfb8afeb95d5e2082ef9d
BurchuMikhail <141926144+BurchuMikhail@users.noreply.github.com>
2025-05-29 17:23:12 +0300

    refactor: upgrade classes

--- models.py
+++ models.py
@@ -8,9 +8,20 @@ class StatusInfo(BaseModel):
 class Room(BaseModel):
     name: str
     devices: List[str] = []
-    
-class RoomResponse(BaseModel):
-    __root__: Dict[UUID, Room]
+
+    def post_response(self, room_id: UUID) -> Dict[UUID, Dict]:
+        return {
+            room_id: {
+                "name": self.name,
+                "devices": []
+            }
+        }
+
+    def put_response(self) -> Dict:
+        return {
+            "name": self.name,
+            "devices": self.devices
+        }
 
 class Capability(BaseModel):
     type: str
@@ -30,9 +41,19 @@ class Device(BaseModel):
     type: str
     capabilities: List[Capability] = []
     properties: List[Property] = []
-    
-class DeviceResponse(BaseModel):
-    __root__: Dict[UUID, Device]
+
+    def post_response(self, device_id: UUID) -> Dict[UUID, Dict]:
+        return {
+            device_id: {
+                "name": self.name,
+                "status_info": self.status_info,
+                "description": self.description,
+                "room_id": self.room_id,
+                "type": self.type,
+                "capabilities": self.capabilities,
+                "properties": self.properties
+            }
+        }
 
 class RoomChange(BaseModel):
     room_id: str
