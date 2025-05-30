66d56d72d33e1fc9163bafa87b8f5c26a77d1bc7
BurchuMikhail <141926144+BurchuMikhail@users.noreply.github.com>
2025-05-30 14:03:07 +0300

    refactor: change-configuration-file-structure (#9)
    * refactor: change configuration file structure
    
    * refactor: change models to fit new config format
    
    * style: remove extra spaces
    
    * feat: add exception handling when working with the configuration file
    
    * refactor: edit functions for new configuration file structure
    
    * feat: add room change endpoint
    
    * style: remove extra spaces
    
    * fix: remove non-existent class
    
    * fix: remove non-existent class
    
    * fix: change call to the correct format
    
    * fix: model fix
    
    * fix: response fix
    
    * fix: fix request to delete room
    
    * fix: fix the element reference error
    
    * fix: add device update entry in the config
    
    * fix: fix the device transfer bug
    
    * fix: fix error of re-saving room with same name
    
    * feature: add error code
    
    * fix: typo correction
    
    * fix: del code in message
    
    * fix: fix status_code error
    
    * feature: add parameter field to model
    
    * fix: remove typo
    
    * Update models.py
    
    * refactor: improve class usage
    
    * refactor: remove an unused class
    
    * refactor: upgrade classes
    
    * refactor: upgrade classes

--- models.py
+++ models.py
@@ -1,36 +1,39 @@
-from typing import List, Optional
+from typing import List, Dict, Optional
 from pydantic import BaseModel
+from uuid import UUID
 
 class StatusInfo(BaseModel):
     reportable: bool = False
 
-class AddRoom(BaseModel):
-    name: str
-
 class Room(BaseModel):
-    id: str
     name: str
     devices: List[str] = []
+
+    def post_response(self, room_id: UUID) -> Dict[UUID, Dict]:
+        return {
+            room_id: {
+                "name": self.name,
+                "devices": []
+            }
+        }
+
+    def put_response(self) -> Dict:
+        return {
+            "name": self.name,
+            "devices": self.devices
+        }
 
 class Capability(BaseModel):
     type: str
     mqtt: str
+    parameters: Optional[dict] = None
 
 class Property(BaseModel):
     type: str
     mqtt: str
-
-class AddDevice(BaseModel):
-    name: str
-    status_info: Optional[dict] = None
-    description: Optional[str] = None
-    room_id: str
-    type: str
-    capabilities: List[Capability] = []
-    properties: List[Property] = []
+    parameters: Optional[dict] = None
 
 class Device(BaseModel):
-    id: str
     name: str
     status_info: Optional[dict] = None
     description: Optional[str] = None
@@ -39,6 +42,22 @@ class Device(BaseModel):
     capabilities: List[Capability] = []
     properties: List[Property] = []
 
+    def post_response(self, device_id: UUID) -> Dict[UUID, Dict]:
+        return {
+            device_id: {
+                "name": self.name,
+                "status_info": self.status_info,
+                "description": self.description,
+                "room_id": self.room_id,
+                "type": self.type,
+                "capabilities": self.capabilities,
+                "properties": self.properties
+            }
+        }
+
+class RoomChange(BaseModel):
+    room_id: str
+
 class Config(BaseModel):
-    rooms: List[Room]
-    devices: List[Device]
+    rooms: Dict[str, Room]
+    devices: Dict[str, Device]
