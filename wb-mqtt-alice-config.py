0cc8aeac3170a49cbae43e8fffb7fcb03a3ae740
victor.fedorov <victor.fedorov@wirenboard.com>
2025-11-14 14:49:40 +0300

    fix: revert formatter for alice-config

--- wb-mqtt-alice-config.py
+++ wb-mqtt-alice-config.py
@@ -27,7 +27,7 @@ app = FastAPI(
 )
 
 # Setting up the logger
-logging.basicConfig(level=logging.INFO, format="%(asctime)s  %(levelname)-8s %(name)s — %(funcName)s -  %(message)s", force=True)
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s", force=True)
 logging.captureWarnings(True)
 logger = logging.getLogger(__name__)
 
